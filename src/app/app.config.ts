9d6b1adf09aee484b066b7b37ce2134258a50ea1
Adam Gasiorek <adam.bjp@gmail.com>
2024-05-25 12:11:54 +0200

    init firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,15 @@ import {ApplicationConfig, provideExperimentalZonelessChangeDetection, provideZo
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+     provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())
   ]
 };
